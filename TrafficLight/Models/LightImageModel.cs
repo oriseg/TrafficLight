80ec5be7a154c85272f23e832caff3119d3bcb6f
GCS08 <shaysol1233@gmail.com>
2025-09-15 21:46:41 +0300

    more comments

--- TrafficLight/Models/LightImageModel.cs
+++ TrafficLight/Models/LightImageModel.cs
@@ -1,8 +1,11 @@
 ﻿namespace TrafficLight.Models
 {
+    //The Model that implements the LightImage changing. ABSTRACT, so there'll be a ModelsLogic.LightImage that inherits from it.
     internal abstract class LightImageModel
     {
+        //An array with the names of the images, so ModelsLogic.LightImage will use it. Protected, so ModelsLogic.LightImage will have it override.
         protected string[] lightImages = ["crysmiley.jpg", "thinksmile.jpg", "happeysmile.jpg", "thinksmile2.jpg"];
+        //A method that returns the name of the image we need to display now, based on the current state from the TrafficLightModel. abstract.
         public abstract string GetLightImage(TrafficLightModel.TrafficLightState state);
     }
 }
--- TrafficLight/Models/LightModel.cs
+++ TrafficLight/Models/LightModel.cs
@@ -1,9 +1,10 @@
 ﻿namespace TrafficLight.Models
 {
-    abstract class LightModel(Color color, bool isOn)
+    //A class that simply represents a Light. abstract, hinting we will also have a ModelsLogin.Light.
+    abstract class LightModel(Color color, bool isOn)//Primary constructor, just adding the constructor to the header.
     {
-        public bool isOn { get; set; } = isOn;
-        protected Color color = color;
-        public abstract Color Color { get; }
+        public bool isOn { get; set; } = isOn;//isOn is a property that can be acheived (get) by other classes and also be set by them. its initial value is the isOn appears in the header.
+        protected Color color = color;//color is a protected type color that defines the color of the light when its on. 
+        public abstract Color Color { get; }//defines the color property that'll be used in the ModelsLogin.Light, the class that inherits from it.
     }
 }
