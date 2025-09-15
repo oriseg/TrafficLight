3f56a8f9d5c547835a79a0366a6d4ebdc63bf627
GCS08 <shaysol1233@gmail.com>
2025-09-15 22:59:15 +0300

    more comments!!!!!

--- TrafficLight/Models/Strings.cs
+++ TrafficLight/Models/Strings.cs
@@ -1,5 +1,8 @@
 ﻿namespace TrafficLight.Models
 {
+    //The ideal in apps is to not write any text that will be displayed in the app in the xml code or other tags code.
+    //For that, we have this class. It's a static class, means none of it values can be changed, where we just write our text.
+    //Whatever we write here is retrieved in the xml.
     static class Strings
     {
         public const string changeLightText = "Change Light";
--- TrafficLight/ModelsLogic/LightImage.cs
+++ TrafficLight/ModelsLogic/LightImage.cs
@@ -2,8 +2,11 @@
 
 namespace TrafficLight.ModelsLogic
 {
+    //The model login of the LightImage. inherits from LightImageModel.
+    //This class job is to keep the displayed image updated.
     internal class LightImage : LightImageModel
     {
+        //A method that returns the name of the image we need to display now, based on the current state from the TrafficLightModel. override.
         public override string GetLightImage(TrafficLightModel.TrafficLightState state)
         {
             return lightImages[(int)state];
